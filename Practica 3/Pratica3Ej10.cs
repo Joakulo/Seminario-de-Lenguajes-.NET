fd2432b876d4498e9a33a7bde36c63b83771d09e
Joaquin Emanuel Santa Cruz <79953847+Joakulo@users.noreply.github.com>
2023-05-09 15:50:41 -0300

    Delete Practica 3 directory

--- Practica 3/Pratica3Ej10.cs
+++
@@ -1,9 +0,0 @@
-﻿double pi = 3.141592653589793;
-
-string format = String.Format("{0:F4}, {0:0.0000}, {0:.####}", pi);
-Console.WriteLine(format);
-
-string interpolado = $"{pi:F4}, {pi:0.0000}, {pi:.####}";
-Console.WriteLine(interpolado);
-
-Console.ReadKey();
