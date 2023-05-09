c3b94440fc6986808e5bf0a01e252f48581796b3
Joaquin Emanuel Santa Cruz <79953847+Joakulo@users.noreply.github.com>
2023-05-09 15:50:59 -0300

    Delete Practica 5 directory

--- Practica 5/Practica5Ej11.cs
+++
@@ -1,5 +0,0 @@
-class Pratica7Ej11
-{
-    public int X = 3;       // Declaracion e inicializacion de una variable
-    public int Y => 3;      // Propiedad publica de solo lectura, simepre devuelve 3
-}
