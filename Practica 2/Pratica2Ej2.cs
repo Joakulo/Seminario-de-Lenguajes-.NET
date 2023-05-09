04071d0f771c9bb46632fc275a7967c5ea27253b
Joaquin Emanuel Santa Cruz <79953847+Joakulo@users.noreply.github.com>
2023-05-09 15:37:41 -0300

    Delete Practica 2 directory

--- Practica 2/Pratica2Ej2.cs
+++
@@ -1,6 +0,0 @@
-﻿object o1 = "A";
-object o2 = o1;
-o2 = "Z";           // Genera un nuevo espacio en Memoria Heap
-Console.WriteLine(o1 + " " + o2);
-
-Console.ReadKey();
--- Practica 2/Pratica2Ej4.cs
+++
@@ -1,4 +0,0 @@
-﻿Console.WriteLine(args == null);    //False
-Console.WriteLine(args.Length);     //0
-
-Console.ReadKey();
